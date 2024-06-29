0ccc69709aa511d67a5bc0aa4cdba19bbc96ac7d
anwarhakim31 <anwarhakim31@gmail.com>
2024-06-29 21:27:20 +0700

    home page  done

--- frontend/src/components/router/Routers.jsx
+++ frontend/src/components/router/Routers.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../../pages/home";
+import ServicePage from "../../pages/servies";
 
 const Routers = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/home" element={<HomePage />} />
+      <Route path="/services" element={<ServicePage />} />
     </Routes>
   );
 };
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -5,6 +5,7 @@ import AboutSection from "../components/fragments/home/AboutSection";
 import ServiceSection from "../components/fragments/home/ServiceSection";
 import FeatureSection from "../components/fragments/home/FeatureSection";
 import OurdoctorSection from "../components/fragments/home/OurdoctorSection";
+import FaqSection from "../components/fragments/home/FaqSection";
 
 const HomePage = () => {
   return (
@@ -15,6 +16,7 @@ const HomePage = () => {
       <ServiceSection />
       <FeatureSection />
       <OurdoctorSection />
+      <FaqSection />
     </>
   );
 };
