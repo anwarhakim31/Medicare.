84c4aa53dec7ea96d570938c30d440ef63f2e12d
anwarhakim31 <anwarhakim31@gmail.com>
2024-07-08 18:33:42 +0700

    finish deploy

--- frontend/src/constant/config.js
+++ frontend/src/constant/config.js
@@ -1,3 +1,3 @@
-export const URL = "medicare-backend-eight.vercel.app/api/v1";
+export const URL = "https://medicare-backend-eight.vercel.app/api/v1";
 
 export const token = localStorage.getItem("token");
